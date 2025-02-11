a467c374120c9545fd52206021741119f8704e11
Jay <91475458+JayK1tsune@users.noreply.github.com>
2025-02-11 22:20:50 +0000

    Added html code to show entries

--- MyMvsApp/Controllers/DiaryEntriesController.cs
+++ MyMvsApp/Controllers/DiaryEntriesController.cs
@@ -5,22 +5,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MyMvsApp.Data;
+using MyMvsApp.Models;
 
 namespace MyMvsApp.Controllers
 {
 
     public class DiaryEntriesController : Controller
     {
+        private readonly ApplicationDbContext _context;
         private readonly ILogger<DiaryEntriesController> _logger;
 
-        public DiaryEntriesController(ILogger<DiaryEntriesController> logger)
+        public DiaryEntriesController(ApplicationDbContext context, ILogger<DiaryEntriesController> logger)
         {
+            _context = context;
             _logger = logger;
         }
 
+
         public IActionResult Index()
         {
-            return View();
+            List<DiaryEntry> entries = _context.DiaryEntries.ToList();
+            return View(entries);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
