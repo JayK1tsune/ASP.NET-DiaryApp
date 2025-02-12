9f4c246e51909c293531ed4a3adf04a08bbf2290
Jay <91475458+JayK1tsune@users.noreply.github.com>
2025-02-12 21:59:29 +0000

    updated Layout

--- MyMvsApp/obj/Debug/net9.0/MyMvsApp.AssemblyInfo.cs
+++ MyMvsApp/obj/Debug/net9.0/MyMvsApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0177cea0fb82c20b230aceb408641071b911d0df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a467c374120c9545fd52206021741119f8704e11")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
